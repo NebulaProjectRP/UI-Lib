050f219bb0a8f31cfdd21ceff498a46c4513d52d
Gonzalolog <gonzalolog@gmail.com>
2021-12-27 13:03:05 -0300

    Update item.lua

--- lua/nebula/controls/item.lua
+++ lua/nebula/controls/item.lua
@@ -1,1 +1,37 @@
-local a
+local PANEL = {}
+
+function PANEL:Init()
+    self.Icon = vgui.Create("nebula.imgur", self)
+    self.Icon:Dock(FILL)
+end
+
+function PANEL:SetItem(id, isLocal)
+    self.isLocal = isLocal
+    self.Reference = NebulaInv.Items[id]
+    if (isLocal) then
+        self.Item = LocalPlayer():getInventory()[id]
+    end
+
+    if (self.Reference.type == "model") then
+        self.Icon:Remove()
+        self.model = vgui.Create("DModelPanel", self)
+        self.model:SetModel(self.Reference.model)
+        self.model:Dock(FILL)
+    else
+        self.Icon:SetImage(self.Reference.id)
+    end
+end
+
+function PANEL:Allow(kind)
+end
+
+
+function PANEL:Paint(w, h)
+    if (self.Reference) then
+        draw.RoundedBox(4, 0, 0, w, h, NebulaInv.Rarities[self.Reference.rarity])
+    else
+        draw.RoundedBox(4, 0, 0, w, h, Color(75, 75, 75, 200))
+    end
+
+    draw.RoundedBox(4, 1, 1, w - 2, h - 2, Color(235, 235, 235, 50))
+end
